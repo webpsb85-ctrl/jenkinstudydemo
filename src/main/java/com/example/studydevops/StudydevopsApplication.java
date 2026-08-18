61f060bb7fee62c2c459912806bfa9eba12dcfc8
webpsb85-ctrl <webpsb85@gmail.com>
2026-08-18 17:51:26 +0530

    Initial commit for StudyDevOps application

---
+++ src/main/java/com/example/studydevops/StudydevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.studydevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudydevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudydevopsApplication.class, args);
+	}
+
+}
