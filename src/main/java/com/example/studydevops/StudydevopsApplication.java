1f74333ff1a70e1cabfcffb05b726a66963211db
webpsb85-ctrl <webpsb85@gmail.com>
2026-08-22 17:09:10 +0530

    Add CI/CD pipeline

--- src/main/java/com/example/studydevops/StudydevopsApplication.java
+++ src/main/java/com/example/studydevops/StudydevopsApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudydevopsApplication {
 
 	public static void main(String[] args) {
+		
+		
 		SpringApplication.run(StudydevopsApplication.class, args);
 	}
 
